9d84600f8ffb709e99d45bfe98ec086013aff8ba
sasha010 <alexander.izmailovich@gmail.com>
2020-11-20 14:09:50 +0200

    add frot

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <span v-bind:class="{done: todo.done}">
+            <input 
+                type="checkbox" 
+                v-on:change="$emit('change-todo-status', todo.id, !todo.done)" 
+                :checked="todo.done">
+            <span class="todo-title">{{todo.todo_title}}</span>
+        </span>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['todo']
+}
+</script>
+
+<style scoped>
+    li {
+        list-style-type: none;
+        padding: 3px 0;
+    }
+    .done {
+        text-decoration: line-through;
+    }
+    .todo-title {
+        padding-left: 20px;
+        font-size: 17px;
+    }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="todo-list">
+        <ul>
+            <TodoItem
+                v-for="todo of todos"
+                v-bind:todo="todo"
+                v-bind:key="todo.id"
+                v-on:change-todo-status="changeTodoStatus"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from './ToDoItem'
+export default {
+    props: ['todos'],
+    components: {
+        TodoItem,
+    },
+    methods: {
+        changeTodoStatus(id, status) {
+            this.$emit('onChangeTodoStatus', id, status)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-list {
+        margin-top: 30px;
+        padding: 5px;
+        overflow-y: auto;
+    }
+    ul {
+        margin-left: 0;
+        padding-left: 0;
+    }
+</style>
